33373f238af949719f81f36338884d4f837f38b1
Abdalla Suliman <abdalla@MacOSs-MacBook-Pro.local>
2023-05-20 11:31:25 +0800

    create workout route and modal

--- server.js
+++ server.js
@@ -1,21 +1,31 @@
 require('dotenv').config()
 
-const express = require('express');
+const express = require('express')
+const mongoose = require('mongoose')
+const workoutRoutes = require('./routes/workouts')
 
 // express app
 const app = express()
 
 // middleware
+app.use(express.json())
+
 app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
 })
 
 // routes
-app.get('/', (req, res) => {
-    res.json({ msg: 'Welcome to the app'})
-})
-//  listen for requests
-app.listen(process.env.PORT, () => {
-    console.log('Hello World!!!!', process.env.PORT)
-})
+app.use('/api/workouts', workoutRoutes)
+
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        //  listen for requests
+        app.listen(process.env.PORT, () => {
+        console.log('Hello World!!!!', process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
